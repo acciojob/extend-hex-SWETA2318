3b3c63f57d4a4550ca12a921c0c18c967c89ea3e
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-25 19:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const extendHex = (shortHex) => {
-  // write your code here
-	const extendHex = (shortHex) => {
-		//shortHex = "#aBo"
-		let i = shortHex . length - 3, isCapital = false;
-		let output = "#";
-		while(i < shortHex[i]. charcodeAt(0));
-		if(asciicode >= 65 && asciicode <= 90) {
-			isCapital = true;
-		}
-		//output = "#" += "a" + "a" =>"#aa"
-		output += shortHex[i] + shortHex[i];
-		i++;
-	}
-	return isCapital : output.toUpperCase() :output;
-};
+function extendHex(shortHex) {
+  // Remove '#' if it exists
+  shortHex = shortHex.replace('#', '');
+
+  // Make sure the input is a valid short hex code
+  if (shortHex.length !== 3 && shortHex.length !== 6) {
+    console.error('Invalid short hex code');
+    return null;
+  }
+
+  // If the input is a short hex code with 3 characters, duplicate each character
+  if (shortHex.length === 3) {
+    shortHex = shortHex.split('').map(char => char + char).join('');
+  }
+
+  // Add '#' prefix and return the extended hex code
+  return '#' + shortHex.toUpperCase();
+}
+
+// Examples
+console.log(extendHex("#abc"));   // Output: "#aabbcc"
+console.log(extendHex("abc"));    // Output: "#aabbcc"
+console.log(extendHex("#AbC"));   // Output: "#AABBCC"
+console.log(extendHex("#f09"));   // Output: "#ff0099"
 
  Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
